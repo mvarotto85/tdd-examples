1dd91c1b6a0ea3b2cbf6747e0575b7a4abb07d32
teo1985 <matteo.varotto@gmail.com>
2018-09-13 17:46:37 +0200

    shouldBeAbleToAddNewKey test PASSING

--- src/test/java/it/tdd/devclass/cache/LRUCacheTests.java
+++ src/test/java/it/tdd/devclass/cache/LRUCacheTests.java
@@ -1,20 +1,29 @@
 package it.tdd.devclass.cache;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class LRUCacheTests {
 
+
+    private LRUCache<String, String> lruCache;
+
+    @Before
+    public void setUp() throws Exception {
+        lruCache = new LRUCache<>();
+    }
+
     @Test
     public void shouldBeAbleToAddNewKey() {
-        LRUCache<String, String> lruCache = new LRUCache<>();
         String expectedKey = "key";
         String expectedValue = "value";
 
         lruCache.add(expectedKey,expectedValue);
 
         assertEquals(expectedValue, lruCache.get(expectedKey));
-
     }
+
+
 }
