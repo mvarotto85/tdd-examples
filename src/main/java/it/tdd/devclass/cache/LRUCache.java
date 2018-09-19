7a1bc6f021b293ea6b52ff03bea1b3f28416075e
teo1985 <matteo.varotto@gmail.com>
2018-09-19 16:26:12 +0200

    should remove least recently used key when exceed capacity --FAILING!

--- src/main/java/it/tdd/devclass/cache/LRUCache.java
+++ src/main/java/it/tdd/devclass/cache/LRUCache.java
@@ -1,9 +1,16 @@
 package it.tdd.devclass.cache;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class LRUCache<K, V> {
+    @Getter
+    @Setter
+    private int capacity;
+
     Map<K,V> cache = new HashMap<>();
 
     public void add(K k, V v) {
@@ -12,5 +19,13 @@ public class LRUCache<K, V> {
 
     public V get(K k) {
         return cache.get(k);
+    }
+
+    public int keyCardinality() {
+        return cache.size();
+    }
+
+    public boolean containsKey(K key) {
+        return cache.containsKey(key);
     }
 }
--- src/test/java/it/tdd/devclass/cache/LRUCacheTests.java
+++ src/test/java/it/tdd/devclass/cache/LRUCacheTests.java
@@ -3,7 +3,7 @@ package it.tdd.devclass.cache;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class LRUCacheTests {
 
@@ -26,4 +26,24 @@ public class LRUCacheTests {
     }
 
 
+    @Test
+    public void shouldRemoveLeastRecentlyUsedKeyWhenExceedCapacity() {
+        lruCache.setCapacity(4);
+
+        lruCache.add("k1","v1");
+        lruCache.add("k2","v2");
+        lruCache.add("k3","v3");
+        lruCache.add("k4","v4");
+
+        assertEquals(4,lruCache.keyCardinality());
+
+        lruCache.add("k5","v5");
+
+        assertEquals(4,lruCache.keyCardinality());
+        assertTrue(lruCache.containsKey("k5"));
+        assertFalse(lruCache.containsKey("k1"));
+
+
+
+    }
 }
