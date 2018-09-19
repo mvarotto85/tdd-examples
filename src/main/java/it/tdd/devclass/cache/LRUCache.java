74c56ac540c5b14958ee0f0a24ed86db569645e7
teo1985 <matteo.varotto@gmail.com>
2018-09-19 17:12:29 +0200

    refactoring

--- src/main/java/it/tdd/devclass/cache/LRUCache.java
+++ src/main/java/it/tdd/devclass/cache/LRUCache.java
@@ -3,22 +3,33 @@ package it.tdd.devclass.cache;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.LinkedHashMap;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class LRUCache<K, V> {
     @Getter
     @Setter
     private int capacity;
 
-    Map<K,V> cache = new LinkedHashMap<>();
+    Map<K,V> cache = new HashMap<>();
+    Map<K,Long> lastUseTime = new HashMap<>();
 
     public void add(K k, V v) {
         cache.put(k,v);
+        lastUseTime.put(k,now());
+    }
+
+    private Long now() {
+        return LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli();
     }
 
     public V get(K k) {
-        return cache.get(k);
+        Optional<V> value = Optional.ofNullable(cache.get(k));
+        value.ifPresent((v) -> lastUseTime.put(k,now()));
+        return value.orElse(null);
     }
 
     public int keyCardinality() {
